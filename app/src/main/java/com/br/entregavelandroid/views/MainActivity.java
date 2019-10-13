ee011f45e7fd38b62772dbdfded4450514f21a58
Deivid Moura <deividmourans@hotmail.com>
2019-10-13 11:57:20 -0300

    Main Views - Activity

--- app/src/main/java/com/br/entregavelandroid/views/MainActivity.java
+++ app/src/main/java/com/br/entregavelandroid/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.br.entregavelandroid;
+package com.br.entregavelandroid.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.br.entregavelandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
