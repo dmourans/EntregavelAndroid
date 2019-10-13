ed27a24ea4d0e4f6c5bcfe576b4a75185e2ac6f4
Deivid Moura <deividmourans@hotmail.com>
2019-10-13 11:56:15 -0300

    Restaurante interface

---
+++ app/src/main/java/com/br/entregavelandroid/interfaces/RestauranteOnClick.java
@@ -0,0 +1,8 @@
+package com.br.entregavelandroid.interfaces;
+
+import com.br.entregavelandroid.models.Restaurante;
+
+public interface RestauranteOnClick {
+    void onClick(Restaurante restaurante);
+
+}
